9480ebc2d784829a12adbaf44a0f4d92254583b7
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-22 17:23:44 +0200

    TP02

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,7 +1,5 @@
 package dev;
 
-import com.github.lalyos.jfiglet.FigletFont;
-
 /**
  * Hello world!
  *
@@ -9,9 +7,15 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String asciiArt = FigletFont.convertOneLine("hello");
-        System.out.println(asciiArt);
+       /*  try {
+            String titre = ResourceBundle.getBundle("application").getString("titre");
+            String asciiArt = FigletFont.convertOneLine(titre);
+            System.out.println(asciiArt);
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } */
+        System.out.println("Bravo, je suis la meilleure ! :)");
     }
 
 }
