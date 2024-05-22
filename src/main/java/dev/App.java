f5d2bffb0d6bbbc9d3fd3759700b8c2f53206b19
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-22 17:44:03 +0200

    TP03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,9 @@
 package dev;
 
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -7,14 +11,19 @@ package dev;
 public class App {
 
     public static void main(String[] args) {
-       /*  try {
+        try {
             String titre = ResourceBundle.getBundle("application").getString("titre");
             String asciiArt = FigletFont.convertOneLine(titre);
             System.out.println(asciiArt);
 
+            // affichage de l'environnement
+            String environnement = ResourceBundle.getBundle("application").getString(
+                    "environnement");
+            System.out.println("Environnement : " + environnement);
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
-        } */
+        }
         System.out.println("Bravo, je suis la meilleure ! :)");
     }
 
